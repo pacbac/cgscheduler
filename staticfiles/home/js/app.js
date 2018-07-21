2d24be9db4ee69972fff55dfd5c0e90426d13ef8
pacbac <jclaytonc99@gmail.com>
2018-07-20 21:21:27 -0400

    Added date validation, edit controls

--- staticfiles/home/js/app.js
+++ staticfiles/home/js/app.js
@@ -1,42 +1,91 @@
 var today = new Date();
 var numRows = 0;
 const rowMargin = 2;
+//so if editing is discarded, this will put the last saved entry back into place
+var edits = {}; //list of edits to be sent to server
+var dates = []; //list of dates on scheduler
+
+Date.prototype.toString = function() {
+  return `${this.getMonth()}/${this.getDay()}/${this.getFullYear()}`
+}
 
 $(document).ready(function(){
-  var rowHeight = $(".navbar").height()+rowMargin;
-  numRows = Math.floor(($(".dates").height()-$(".navbar").height())/rowHeight);
-  for(let i = 0; i < numRows; i++)
-    $(".info").append("<div  class='element element"+(i+1)+"'></div>");
+  { //misc UI adjustments
+    let rowHeight = $(".navbar").height()+rowMargin;
+    numRows = Math.floor(($(".dates").height()-$(".navbar").height())/rowHeight);
+    for(let i = 0; i < numRows; i++)
+      $(".info").append("<div  class='element element"+(i+1)+"'></div>");
+  }
 
   loadDates();
+  loadListeners();
+});
 
-  var toggleTextField = function() {
-    if($(this).children("input").hasClass("editField")) return
-    else if($(".element").has(".editField").length){
-      let date = $(".editField").val()
-      $(".editField").parent().html(date)
+function loadListeners() {
+  //toggle text field on clicked element
+  $(document).on("click", ".element", function() {
+    if($(this).children("input").hasClass("editField")) return; //no need to retoggle same element clicked on
+    else if($(".element").has(".editField").length){ //if editField already exists
+      let val = $(".editField").val()
+      $(".editField").parent().html(val)
     }
     let date = $(this).text()
     $(this).html("<input type='text' class='editField'>")
     $(".editField").val(date)
-  }
-  $(document).on("click", ".element", toggleTextField)
-  $(document).on("keypress", ".element", e => {
+  })
+
+  //attempt to save date when user presses enter
+  $(document).on("keypress", ".dates .editField", e => {
     if(e.which == 13){
-      let date = $(".editField").val()
-      $(".editField").parent().html(date)
+      if(checkDateFormat($(".editField").val())){
+        let val = $(".editField").val()
+        let classes = $(".editField").parent().attr("class").split(" ")
+        let index = parseInt(classes[1].charAt(classes[1].length - 1)) - 1
+        console.log(dates[index])
+        $(".editField").parent().html(val)
+        $("button").show()
+      } else
+        alert("Error: Enter a valid date")
     }
   })
+  //attempt to save chenges when user presses enter
+  $(document).on("keypress", ".roles .editField", e => {
+    if(e.which == 13){
+      let val = $(".editField").val()
+      $(".editField").parent().html(val)
+      $("button").show() //show save button
+    }
+  })
+  //select newly clicked element
   $(".navbar :nth-child(n)").click(function() {
     $(".selected").removeClass("selected")
     $(this).addClass("selected")
   })
-});
+
+  $("button[name='cancel']").click(() => location.reload())
+  $("button[name='save']").click(() => {
+    $.post('/post', {edits}, json => {
+
+    })
+  })
+}
+
+function checkDateFormat(str){
+  let splitDate = str.split("/")
+  if(splitDate.length != 3) return false
+  let [month, day, year] = splitDate
+  try {
+    [month, day, year] = [parseInt(month), parseInt(day), parseInt(year)]
+    let testDate = new Date(year, month, day)
+    return year === testDate.getFullYear() && month === testDate.getMonth() && day === testDate.getDate()
+  } catch(e) {
+    console.log(e)
+    return false
+  }
+}
 
 function loadDates(){
-  const thisYr = today.getFullYear(),
-      thisMonth = today.getMonth(),
-      thisDate = today.getDate();
+  const [thisYr, thisMonth, thisDate] = [today.getFullYear(), today.getMonth(), today.getDate()];
 
   //calculate the next 2nd or 4th saturday, given a date
   var getSaturday = date => {
@@ -52,18 +101,18 @@ function loadDates(){
   };
 
   var processDays = (addThisDate, j) => {
-    let addMonth = addThisDate.getMonth(),
-        addDate = addThisDate.getDate(),
-        addYr = addThisDate.getFullYear()
+    let [addMonth, addDate, addYr] = [addThisDate.getMonth(), addThisDate.getDate(), addThisDate.getFullYear()];
     $(`.dates .element${j}`).text(`${addMonth+1}/${addDate}/${addYr}`);
-    if(addMonth == thisMonth && addDate >= thisDate)
+    if(addMonth === thisMonth && addDate >= thisDate)
       $(`.dates .element${j}`).addClass("upcoming");
   }
 
   for(let i = 0, monthInd = 0; i < numRows, monthInd < Math.floor(numRows/2); i+=2, monthInd++){
     let addThisDate = getSaturday(new Date(thisYr, thisMonth+monthInd, 1));
     processDays(addThisDate, i+1)
+    dates.push(addThisDate)
     addThisDate = getSaturday(new Date(thisYr, thisMonth+monthInd, 15));
     processDays(addThisDate, i+2)
+    dates.push(addThisDate)
   }
 }
