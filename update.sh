05991bf7f782cc8787cf8adfd6b6bc67053348bd
pacbac <jclaytonc99@gmail.com>
2018-07-22 18:02:31 -0400

    Add to update.sh

--- update.sh
+++ update.sh
@@ -3,6 +3,9 @@ node-sass assets/styles/style.scss -o staticfiles/home/css 2>&1
 
 if [ $? -eq 0 ]; then
   echo 'SASS finished successfully.'
+else
+  echo 'SASS compilation error'
+  exit 1
 fi
 
 #start django server
