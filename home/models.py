c0476d936f2e17dddc9c11f666586e4553058914
pacbac <jclaytonc99@gmail.com>
2018-07-29 17:35:49 -0400

    Add youth field

--- home/models.py
+++ home/models.py
@@ -9,6 +9,7 @@ class TableEdit(models.Model):
     place = models.CharField(max_length=20, blank=True, null=True)
     moderator = models.CharField(max_length=20, blank=True, null=True)
     topic = models.CharField(max_length=75, blank=True, null=True)
+    youth = models.CharField(max_length=20, blank=True, null=True)
     children = models.CharField(max_length=20, blank=True, null=True)
     remarks = models.CharField(max_length=100, blank=True, null=True)
 
@@ -18,16 +19,18 @@ class TableEdit(models.Model):
         if self.place: propArr.append("Place: " + self.place)
         if self.moderator: propArr.append("Mod: " + self.moderator)
         if self.topic: propArr.append("Topic: " + self.topic)
+        if self.youth: propArr.append("Youth: " + self.youth)
         if self.children: propArr.append("Children: " + self.children)
         if self.remarks: propArr.append("Remarks: " + self.remarks)
         return ", ".join(propArr)
 
     def toDict(self):
         d = {}
-        if self.correctDate: d['newDate'] = self.correctDate
+        if self.correctDate: d['newDate'] = date_utils.dateToStr(self.correctDate)
         if self.place: d['place'] = self.place
         if self.moderator: d['moderator'] = self.moderator
         if self.topic: d['topic'] = self.topic
+        if self.youth: d['youth'] = self.youth
         if self.children: d['children'] = self.children
         if self.remarks: d['remarks'] = self.remarks
         return d
