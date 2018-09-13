2defd646c40ca9dedc8f65288d6308cc3df48f0b
pacbac <jclaytonc99@gmail.com>
2018-09-13 12:56:42 -0400

    Add row/column checks for table entries

--- frontend/src/containers/TableEdit.js
+++ frontend/src/containers/TableEdit.js
@@ -35,9 +35,9 @@ class TextField extends Component {
       && i !== undefined
       && category !== undefined
       && this.state.content !== ""){
-        store.dispatch(editElem(this.state.content, this.props.position))
-        store.dispatch(editAPITableElem(this.state.content, this.props.position))
-      }
+      store.dispatch(editElem(this.state.content, this.props.position))
+      store.dispatch(editAPITableElem(this.state.content, this.props.position))
+    }
   }
 
   render(){
@@ -53,36 +53,73 @@ class TextField extends Component {
 class Dropdown extends Component {
   constructor(props){
     super(props)
-    this.handleChange = this.handleChange.bind(this)
     let {year, i, category} = this.props.position
     let tblEntries = store.getState().tableEntries
     const flattenedKey = [year, i, category].join(", ")
     const content = tblEntries[flattenedKey] || ''
     this.state = { content }
+    this.handleChange = this.handleChange.bind(this)
+    this.checkRowErr = this.checkRowErr.bind(this)
+    this.checkColErr = this.checkColErr.bind(this)
   }
 
   handleChange(e){
-    this.setState({content: e.target.value})
+    this.setState({ content: e.target.value })
+    // notify of potential row/column edit conflicts to the user when setting a new value
+    // however, whether the conflicts are actual problems depends on the user.
+    this.checkRowErr(e.target.value)
+    this.checkColErr(e.target.value)
+    // the user may save the table while the dropdown still exists
+    // so we assume the currently selected value is what they want
+    store.dispatch(editAPITableElem(e.target.value, this.props.position))
+  }
+
+  // @param val: the new value of the dropdown (that hasn't been officially changed yet)
+  checkRowErr(val){
+    let { year, i, category } = this.props.position
+    if(category == "place") return //place should be omitted from evaluation
+    if(val == "Canceled" || val == "") return //"Canceled" doesn't count as duplicate
+    let tblEntries = store.getState().tableEntries
+    if(category == "youth" || category == "children"){
+      // if not the first item, check if the item above is the same value
+      if(tblEntries[`${year}, ${i}, moderator`] === val)
+        alert(`Warning: ${val} has conflicts with other roles for ${tblEntries[`${year}, ${i}, dates`]}.`)
+    } else if(category == "moderator"){
+      if(tblEntries[`${year}, ${i}, children`] === val || tblEntries[`${year}, ${i}, youth`] === val)
+        alert(`Warning: ${val} has conflicts with other roles for ${tblEntries[`${year}, ${i}, dates`]}.`)
+    }
+  }
+
+  // @param val: the new value of the dropdown (that hasn't been officially changed yet)
+  checkColErr(val){
+    let { year, i, category } = this.props.position
+    if(category == "place") return //place should be omitted from evaluation
+    if(val == "Canceled" || val== "") return//"Canceled" doesn't count as duplicate
+    let tblEntries = store.getState().tableEntries
+    if(i > 0 && tblEntries[`${year}, ${i-1}, ${category}`] === val)
+      alert(`Warning: ${val} is assigned consecutively to ${category} on ${tblEntries[`${year}, ${i-1}, dates`]} and ${tblEntries[`${year}, ${i}, dates`]}.`)
+    if(i < 23 && tblEntries[`${year}, ${i+1}, ${category}`] === val)
+      alert(`Warning: ${val} is assigned consecutively to ${category} on ${tblEntries[`${year}, ${i}, dates`]} and ${tblEntries[`${year}, ${i+1}, dates`]}.`)
   }
 
   componentWillUnmount(){
     store.dispatch(editElem(this.state.content, this.props.position))
-    store.dispatch(editAPITableElem(this.state.content, this.props.position))
   }
 
   render(){
     let curState = store.getState()
     let yr = curState.tabs.selectedYr
     let options = curState.entriesPool[`${yr}, ${this.props.position.category}`] || []
-    options.push("")
-    options.push("Canceled")
-    options = Array.from(new Set(options)) // remove duplicates
+    // remove duplicates and remove potential '' and 'Canceled' options if they exist
+    options = Array.from(new Set(options)).filter(option => option != '' && option != 'Canceled')
     return (
       <select className="edit-field" onChange={this.handleChange}>
+        <option value=''></option>
         {options.map(elem => (
           <option value={elem}
             selected={this.state.content === elem}>{elem}</option>)
         )}
+        <option value='Canceled'>Canceled</option>
       </select>
     )
   }
